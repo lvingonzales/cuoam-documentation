69a0238ea2f5f8733b27300cc05b5e6a83de0f4f
lvingonzales <lvingonzales15@gmail.com>
2026-04-18 00:48:17 -0400

    Finished all views and polish

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // Set up Express app
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// List of valid routes
+app.locals.validRoutes = ['/', '/concept', '/art', '/technical_info', '/about'];
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -16,6 +20,34 @@ app.listen(port, () => {
 app.use(express.static('public/css'));
 app.use(express.static('public/images'));
 app.use(express.static('public/js'));
+
+// Use cookie parser middleware
+app.use(cookieParser());
+app.use(express.json());
+
+app.use ((req, res, next) => {
+  res.locals.theme = req.cookies.theme || 'light';
+  next();
+});
+
+app.post('/set-theme', (req, res) => {
+  const theme = req.body.theme === 'dark' ? 'dark' : 'light';
+  res.cookie('theme', theme, {maxAge: 9000000, httpOnly: true});
+  res.json({ success: true });
+});
+
+// Set the current path
+app.use((req, res, next) => {
+  res.locals.currentPath = req.path;
+
+  res.locals.navLink = (path, label) => {
+    const isActive = req.path === path;
+    const activeClass = isActive ? 'active' : '';
+    const ariaCurrent = isActive ? 'aria-current="page"' : '';
+    return `<li class="nav-item px-4"><a class="nav-link pe-4 ${activeClass}" href="${path}" ${ariaCurrent}>${label}</a></li>`
+  }
+  next();
+})
 
 // Routing
 app.get('/', (req, res) => {
